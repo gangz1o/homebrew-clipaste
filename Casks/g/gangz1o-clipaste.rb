ccfbdf334d6dbf63e685b60f03888c41c6fccdf1
gangz1o <gangz1o@qq.com>
2026-09-11 02:44:22 +0000

    Update gangz1o-clipaste to 2.2.14

--- Casks/g/gangz1o-clipaste.rb
+++ Casks/g/gangz1o-clipaste.rb
@@ -1,6 +1,6 @@
 cask "gangz1o-clipaste" do
-  version "2.2.13"
-  sha256 "f42eb5a4457cb9cbfcbb180bf52d3726e7dea79a5172b5edb90e1bffc6d2ad3d"
+  version "2.2.14"
+  sha256 "12ed964038c73109120d27c0aa2476fd808e5a035ec695fe1e36c4d622489deb"
 
   url "https://github.com/gangz1o/Clipaste/releases/download/v#{version}/Clipaste-v#{version}.dmg"
   name "Clipaste"
